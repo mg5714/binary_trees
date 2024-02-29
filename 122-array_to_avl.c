be6cba8675cdafe6a5b8d19c0de392cb60d92587
mg5714 <mistergddora2000@gmail.com>
2024-02-29 19:55:37 +0200

    try

--- 122-array_to_avl.c
+++ 122-array_to_avl.c
@@ -28,7 +28,7 @@ avl_t *array_to_avl(int *array, size_t size)
 		}
 		if (j == i)
 		{
-			if (avl_insert(&tree, array[i]) == NULL)
+			if (avl_insert(&root, array[i]) == NULL)
 				return (NULL);
 		}
 	}
